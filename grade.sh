5d5ce964e487b1421e45b869162e7931b2a89014
stbui02 <stbui@ucsd.edu>
2024-03-07 10:49:17 -0800

    Grade.sh pushPlease enter the commit message for your changes. Lines starting

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,6 +8,28 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+pwd 
+if ! [ -f student-submission/ListExamples.java ]
+then
+    echo "Missing Necessary Files"
+    exit
+fi 
+echo "Continue"
+
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+pwd
+
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then 
+    echo "Compilation Error"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
